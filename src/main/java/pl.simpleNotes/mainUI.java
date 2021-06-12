e882f7e0462acbae09d74d49df94823b6ee477d2
Bamboooz <BambusixMc@gmail.com>
2021-06-12 19:57:51 +0200

    commit

--- src/main/java/pl.simpleNotes/mainUI.java
+++ src/main/java/pl.simpleNotes/mainUI.java
@@ -2,12 +2,18 @@ package pl.simpleNotes;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.Objects;
 
 import static pl.simpleNotes.makeNewNote.*;
 import static pl.simpleNotes.noteGUI.*;
 
 class mainUI {
+
+    public static String runEverything = "";
+
     public static void main(String[] args) {
 
         JFrame frame = new JFrame("| Simple Notes |");
@@ -43,10 +49,52 @@ class mainUI {
         newNote.setBounds(64, 220, 200, 200);
         newNote.setBorderPainted(true);
 
+        JButton openNote = new JButton("Open Note");
+        frame.add(openNote);
+        openNote.setVisible(true);
+        openNote.setBackground(bgColor);
+        openNote.setBounds(64, 440, 200, 50);
+        openNote.setBorderPainted(true);
+
         newNote.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 createNewNote();
                 frame.setVisible(false);
+            }
+        });
+
+        openNote.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                JFileChooser fileChooser=new JFileChooser();
+                int a=fileChooser.showOpenDialog(null);
+                if(a==JFileChooser.APPROVE_OPTION)
+                {
+                    File fileToOpen = fileChooser.getSelectedFile();
+                    try
+                    {
+                        newNoteName = fileToOpen;
+                        frame.setTitle("| Simple Notes, Note: " + "'" + newNoteName.toString() + "'" + " |");
+
+                        try (BufferedReader br = new BufferedReader(new FileReader(fileToOpen))) {
+                            StringBuilder sb = new StringBuilder();
+                            String line = br.readLine();
+
+                            while (line != null) {
+                                sb.append(line);
+                                sb.append("\n");
+                                line = br.readLine();
+                            }
+                            runEverything = sb.toString();
+                            ifNoteOpened();
+                        } finally {
+                            frame.setVisible(false);
+                        }
+                    }
+                    catch(Exception exception)
+                    {
+                        System.out.println("Problem found when to open the file");
+                    }
+                }
             }
         });
 
--- src/main/java/pl.simpleNotes/noteGUI.java
+++ src/main/java/pl.simpleNotes/noteGUI.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.io.*;
 import java.util.Objects;
 
+import static pl.simpleNotes.mainUI.runEverything;
 import static pl.simpleNotes.makeNewNote.newNoteName;
 
 public class noteGUI {
@@ -36,8 +37,13 @@ public class noteGUI {
 
         JTextArea noteText = new JTextArea();
         noteText.setVisible(true);
-        JScrollPane scrollBar = new JScrollPane(noteText);
-        scrollBar.setBounds(0, 0, 728, 860);
+        JScrollPane scrollBar = new JScrollPane(noteText,
+                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollBar.setBorder(BorderFactory.createTitledBorder("Note Text:"));
+        noteText.setLineWrap(true);
+        noteText.setWrapStyleWord(true);
+        scrollBar.setBounds(0, 0, 728, 880);
         frame.add(scrollBar);
 
         final JMenuItem saveAs = new JMenuItem("Save As:");
@@ -53,7 +59,6 @@ public class noteGUI {
                 try {
                     File txtFile = new File (filePath, fileText);
                     if (txtFile.createNewFile()) {
-                        txtFile.delete();
                         System.out.println("File created: " + txtFile.getName());
                         FileWriter myWriter = new FileWriter(new File(filePath, String.valueOf(newNoteName)));
                         myWriter.write(fileText);
@@ -81,9 +86,8 @@ public class noteGUI {
                 {
                     newNoteName = fileToOpen;
                     frame.setTitle("| Simple Notes, Note: " + "'" + newNoteName.toString() + "'" + " |");
-                    BufferedReader br = new BufferedReader(new FileReader(fileToOpen));
 
-                    try {
+                    try (BufferedReader br = new BufferedReader(new FileReader(fileToOpen))) {
                         StringBuilder sb = new StringBuilder();
                         String line = br.readLine();
 
@@ -94,8 +98,6 @@ public class noteGUI {
                         }
                         String everything = sb.toString();
                         noteText.setText(everything);
-                    } finally {
-                        br.close();
                     }
                 }
                 catch(Exception exception)
@@ -106,6 +108,106 @@ public class noteGUI {
         }));
 
         menu.add(openFile);
+
+    }
+
+    public static void ifNoteOpened() {
+
+            JFrame frame = new JFrame("| Simple Notes |");
+
+            Image icon = Toolkit.getDefaultToolkit().getImage(Objects.requireNonNull(mainUI.class.getClassLoader().getResource("Graphics/notepadIcon.png")));
+            frame.setIconImage(icon);
+
+            frame.pack();
+            frame.setVisible(true);
+            frame.setLayout(null);
+            frame.setLocationRelativeTo(null);
+            frame.setResizable(false);
+            frame.setTitle("| Simple Notes, Note: " + "'" + newNoteName + "'" + " |");
+            frame.setSize(748, 880);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            JMenuBar utilsMenu = new JMenuBar();
+            utilsMenu.setVisible(true);
+            frame.add(utilsMenu);
+            frame.setJMenuBar(utilsMenu);
+            JMenu menu = new JMenu("File");
+            utilsMenu.add(menu);
+
+            JTextArea noteText = new JTextArea();
+            noteText.setVisible(true);
+            noteText.setText(runEverything);
+            JScrollPane scrollBar = new JScrollPane(noteText,
+                    JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+                    JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+            scrollBar.setBorder(BorderFactory.createTitledBorder("Note Text:"));
+            noteText.setLineWrap(true);
+            noteText.setWrapStyleWord(true);
+            scrollBar.setBounds(0, 0, 728, 880);
+            frame.add(scrollBar);
+
+            final JMenuItem saveAs = new JMenuItem("Save As:");
+            saveAs.addActionListener((e -> {
+
+                JFileChooser f = new JFileChooser();
+                f.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                f.showSaveDialog(null);
+
+                filePath = (f.getSelectedFile());
+                fileText = noteText.getText();
+
+                try {
+                    File txtFile = new File (filePath, fileText);
+                    if (txtFile.createNewFile()) {
+                        System.out.println("File created: " + txtFile.getName());
+                        FileWriter myWriter = new FileWriter(new File(filePath, String.valueOf(newNoteName)));
+                        myWriter.write(fileText);
+                        myWriter.close();
+                    } else {
+                        System.out.println("File already exists.");
+                    }
+                } catch (IOException exept) {
+                    System.out.println("An error occurred.");
+                    exept.printStackTrace();
+                }
+
+            }));
+
+            menu.add(saveAs);
+
+            final JMenuItem openFile = new JMenuItem("Open File:");
+            openFile.addActionListener((e -> {
+                JFileChooser fileChooser=new JFileChooser();
+                int a=fileChooser.showOpenDialog(null);
+                if(a==JFileChooser.APPROVE_OPTION)
+                {
+                    File fileToOpen = fileChooser.getSelectedFile();
+                    try
+                    {
+                        newNoteName = fileToOpen;
+                        frame.setTitle("| Simple Notes, Note: " + "'" + newNoteName.toString() + "'" + " |");
+
+                        try (BufferedReader br = new BufferedReader(new FileReader(fileToOpen))) {
+                            StringBuilder sb = new StringBuilder();
+                            String line = br.readLine();
+
+                            while (line != null) {
+                                sb.append(line);
+                                sb.append("\n");
+                                line = br.readLine();
+                            }
+                            String everything = sb.toString();
+                            noteText.setText(everything);
+                        }
+                    }
+                    catch(Exception exception)
+                    {
+                        System.out.println("Problem found when to open the file");
+                    }
+                }
+            }));
+
+            menu.add(openFile);
 
     }
 
